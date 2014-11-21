e68ceaeac220be23a77aa15f61d2d5ef27e534c3
Half-Shot <half-shot@molrams.com>
2014-11-21 23:34:17 +0000

    * Added basic google maps support. * Added a basic location finder (not perfect on desktop).

--- js/main.js
+++ js/main.js
@@ -3,19 +3,20 @@
 // https://developers.google.com/places/documentation/search
 
 //Constant Variables
-var databaseURL = "https://raw.githubusercontent.com/ChelmsfordMakerspace/KeepSafe/master/keepsafe.json";
+var databaseURL = "keepsafe.json";
+var map;
 //window.ksPlaces
-
 //Load the JSON File
 function LoadDatabase(URL){
-	$.getJSON(URL,function( data ){window.ksPlaces = data});
+	$.ajax(URL,{
+	                mimeType:"application/json",
+	                success:function( data )
+				    {
+					    window.ksPlaces = data;
+				    }
+				});
 }
 
-function RenderMap(selector,location = false){
-	var divObject = $(selector)
-}
-
-//
 function GetLocationInformation(Address){
 
 }
@@ -23,5 +24,27 @@ function GetLocationInformation(Address){
 //Main Steps
 //Load the database.
 LoadDatabase(databaseURL);
-//Render the map of the users area. Do not set a location, because GeoLocation will do it for us.
-RenderMap(".googlemap");
+function loadMap(){
+    //Create a maps
+    map = new google.maps.Map($('.googlemap')[0],{zoom: 15});
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            map.setCenter(pos);
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'You are here.'
+            });
+
+        });
+    }
+    else{
+            //Err, dunno.
+            alert("Sorry, we could not find you!"); //Replace this with something less alarming.
+    }
+}
+
+
+google.maps.event.addDomListener(window, 'load', loadMap);
